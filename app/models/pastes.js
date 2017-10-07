0b8c0b962a61378cf1498fc0a304b6624027626d
Galore Analytics <galore@Galores-MacBook-Pro.local>
2017-10-07 13:20:16 -0700

    paste with timeout

--- app/models/pastes.js
+++ app/models/pastes.js
@@ -1,18 +1,21 @@
-const mongoose = require("mongoose"),
-    Schema = mongoose.Schema,
-    bcrypt = require("bcrypt-nodejs");
 
-let PasteSchema = new Schema({
-    title: {
-        type: String
-    },
-    body: {
-        type: String,
-    },
-    password: {
-        type: String,
-    }
-});
+let expiration = 20;
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
+/* user profile information */
+var pasteID = ObjectId();
+
+let pasteSchema = new Schema({
+	id: {type: String},
+	paste_title: {type: String, required: true},
+ 	paste_body: { type: String, required: true },
+ 	password: {type: String},
+  
+}, {timestamps: true});
+
+pasteSchema.index({createdAt: 1},{expireAfterSeconds: expiration});
 
 PasteSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(12), null);
@@ -22,7 +25,12 @@ PasteSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
 };
 
-const Note = mongoose.model("Paste", PasteSchema);
 
-module.exports = Note;
+const Paste = mongoose.model("Paste", postSchema);
+module.exports = Paste;
+
+
+
+
+
 
