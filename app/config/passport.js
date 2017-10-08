22ce65636b6281d4154fcd5a40ad42e91eb1c15a
Namho Kim <nk@namho.kim>
2017-10-07 23:44:30 -0700

    fixed annoying bug & added api routes

--- app/config/passport.js
+++ app/config/passport.js
@@ -16,41 +16,37 @@ passport.deserializeUser(function(obj, cb){
 passport.use("local-signup", new LocalStrategy(
     { usernameField: "username", passwordField: "password", passReqToCallback: true },
     function(req, username, password, done) {
-        email = email.toLowerCase();
-        User.findOne({ "email": email }, (err, user) => {
+        username = username.toLowerCase();
+        User.findOne({ "username": username }, (err, user) => {
             if(err) { return done(err) }
             if(user) {
                 return done(null, false);
-            } else {
-                const newUser = new User();
-                newUser.email = email;
-                newUser.password = newUser.generateHash(password);
-
-                newUser.save(err => {
-                    if(err) { throw err }
-                    return done(null, newUser);
-                });
             }
+            let newUser = new User();
+            newUser.username = username;
+            newUser.password = newUser.generateHash(password);
+            newUser.save(err => {
+                if(err) { throw err }
+                return done(null, newUser);
+            });
         });
     })
 );
 
 passport.use("local-login", new LocalStrategy(
-    { passReqToCallBack: true },
+    { passReqToCallback: true },
     function(req, username, password, done) {
-        email = email.toLowerCase();
+        username = username.toLowerCase();
         User.findOne({ "username": username }, (err, user) => {
             if(err) { throw err }
             if(!user) {
                 return done(null, false);
-            } else if(!User.validPassword(password)) {
+            } else if(!user.validPassword(password)) {
                 return done(null, false);
             }
-
             return done(null, user);
         });
     })
 );
 
 module.exports = passport;
-
--- app/controller/authentication.js
+++ app/controller/authentication.js
@@ -1,40 +1,40 @@
 const passport = require("../config/passport.js");
 
 module.exports = app => {
-    app.post("/login", (req, res, next) => {
-        passport.authenticate("local-login", (err, user, info) => {
-            if(err) { return err }
-            if(!user) { return res.json("failed") }
-            req.logIn(user, err => {
-                if(err) { return err }
-                return res.redirect("back");
-            });
-        })(req, res, next);
-    });
+    // app.post("/login", (req, res, next) => {
+    //     passport.authenticate("local-login", (err, user, info) => {
+    //         if(err) { return err }
+    //         if(!user) { return res.json("failed") }
+    //         req.logIn(user, err => {
+    //             if(err) { return err }
+    //             return res.redirect("/");
+    //         });
+    //     })(req, res, next);
+    // });
 
-    app.post("/register", (req, res, next) => {
-        passport.authenticate("local-signup", (err, user, info) => {
-            if(err) { return err }
-            if(!user) { return res.json("failed") }
-            req.logIn(user, err => {
-                if(err) { return err }
-                return res.redirect("back");
-            });
-        })(req, res, next);
-    });
+    app.post("/login", passport.authenticate("local-login", {
+        successRedirect: "/",
+        failureRedirect: "/login?login=failed",
+    }));
+
+    app.post("/register", passport.authenticate("local-signup", {
+        successRedirect: "/",
+        failureRedirect: "/register?signup=failed"
+    }));
+
+    // app.post("/register", (req, res, next) => {
+    //     passport.authenticate("local-signup", (err, user, info) => {
+    //         if(err) { return err }
+    //         if(!user) { return res.json("failed") }
+    //         req.logIn(user, err => {
+    //             if(err) { return err }
+    //             return res.redirect("/");
+    //         });
+    //     })(req, res, next);
+    // });
 
     app.get("/logout", (req, res) => {
         req.logout();
-        res.redirect("/");
-    });
-
-    app.get("/", (req, res) => {
-        if(req.user) {
-            res.json(req.user);
-        }
-        else {
-            res.json("not logged in");
-        }
+        res.redirect("/login");
     });
 }
-
