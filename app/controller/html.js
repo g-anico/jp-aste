ca001856eba71791fff03eb38c3716465a605d0c
Namho Kim <nk@namho.kim>
2017-10-11 16:53:20 -0700

    example frontends

--- app/controller/html.js
+++ app/controller/html.js
@@ -1,10 +1,11 @@
+const path = require("path");
+
 module.exports = app => {
     app.get("/", (req, res) => {
         if(!req.user) {
             res.redirect("/login");
         } else {
-            // TODO: Update with actual page
-            res.sendFile("/home/nkim/Desktop/CodeBootCamp/jp-aste/test.html");
+            res.sendFile(path.join(__dirname + "/../../views/index.html"));
         }
     });
 
@@ -12,8 +13,7 @@ module.exports = app => {
         if(req.user) {
             res.redirect("/");
         } else {
-            // TODO: Update with actual page
-            res.sendFile("/home/nkim/Desktop/CodeBootCamp/jp-aste/form.html");
+            res.sendFile(path.join(__dirname + "/../../views/login.html"));
         }
     });
 
@@ -21,12 +21,12 @@ module.exports = app => {
         if(req.user) {
             res.redirect("/");
         } else {
-            res.send("");
+            res.sendFile(path.join(__dirname + "/../../views/register.html"));
         }
     });
     
     app.get("/paste/:pasteid", (req, res) => {
         const pasteid = req.params.pasteid;
-        // TODO: set up page
+        res.sendFile(path.join(__dirname + "/../../views/paste.html"));
     });
 }
